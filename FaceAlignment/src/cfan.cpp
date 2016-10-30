edb148fb1ad1ce20caf37fbe16274a6aa027db30
rustch3n <rust.ch3n@gmail.com>
2016-10-30 12:02:38 +0800

    fix ubuntu 1604 build

--- FaceAlignment/src/cfan.cpp
+++ FaceAlignment/src/cfan.cpp
@@ -209,7 +209,7 @@ void CCFAN::FacialPointLocate(const unsigned char *gray_im, int im_width, int im
   {
     for (int j = 0; j < pts_num_; j++)
     {
-      if (isnan(fea[j * 128 + i]))
+      if (std::isnan(fea[j * 128 + i]))
       {
         re_fea[i*pts_num_ + j] = 0;
       }
@@ -285,7 +285,7 @@ void CCFAN::FacialPointLocate(const unsigned char *gray_im, int im_width, int im
   {
     for (int j = 0; j < pts_num_; j++)
     {
-      if (isnan(fea[j * 128 + i]))
+      if (std::isnan(fea[j * 128 + i]))
       {
         re_fea[i*pts_num_ + j] = 0;
       }
