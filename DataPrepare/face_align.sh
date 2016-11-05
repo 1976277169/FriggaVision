c358557cc2fb7de81f947873528baface4409748
rustch3n <bingfeng.ch3n@gmail.com>
2016-11-05 10:11:44 +0800

    face_align with argv

--- DataPrepare/face_align.sh
+++ DataPrepare/face_align.sh
@@ -1,7 +1,11 @@
 #(c) 2016 chenbingfeng
-target_dir=webface-tiny
+if [ $# -ne 1 ]; then
+echo "USAGE $0 be_aligned_dir"
+exit
+fi
+target_dir="$1"
 echo "generate image list"
-find "$target_dir" -name "*.jpg" > image_list.txt
+find "$target_dir" -name "*.jpg" > "$1"_image_list.txt
 
 ../FaceAlignment/build/fa_test ../FaceDetection/model/seeta_fd_frontal_v1.0.bin \
- ../FaceAlignment/model/seeta_fa_v1.1.bin image_list.txt align_result.txt
+ ../FaceAlignment/model/seeta_fa_v1.1.bin "$1"_image_list.txt "$1"_align_result.txt
